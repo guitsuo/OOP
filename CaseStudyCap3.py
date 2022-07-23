4eec66dd9bc928cc97ed0112979cce7b4c16f267
guitsuo <guilhermeoshiro@outlook.com>
2022-07-23 19:54:18 -0300

    adicao dos arquivos

--- CaseStudyCap3.py
+++ CaseStudyCap3.py
@@ -2,19 +2,37 @@
 
 class Propriedade:
 
-    def __init__(self, metros_quadrados, n_camas, n_banheiros):
+    def __init__(self, metros_quadrados, n_camas, n_banheiros, **kwargs):
         self.metros_quadrados = metros_quadrados
         self.n_camas = n_camas
         self.n_banheiros = n_banheiros 
 
+    def display(self):
+        '''
+        Deve representar as características da propriedade
+        '''
+        return f'Esta propriedade possui {self.metros_quadrados}, {self.n_camas} e {self.n_banheiros}'
+
+    @staticmethod
+    def prompt_init():
+        return dict(metros_quadrados = input("Adicione os metros quadrados: "),
+                    camas = input("Adicione o numero de camas"),
+                    banhos = input("Adicione o numero de banheiros")
+        )
+    #prompt_init = staticmethod(prompt_init)
+
+
 class Apartamento(Propriedade):
-    def __init__(self, metros_quadrados, n_camas, n_banheiros):
+    def __init__(self, metros_quadrados, n_camas, n_banheiros, sacada, lavanderia):
         super().__init__(metros_quadrados, n_camas, n_banheiros)
-        self.sacada = self.sacada
-        self.lavanderia = self.lavanderia 
+        self.sacada = sacada
+        self.lavanderia = lavanderia 
+
+class Casa(Propriedade):
+    def __init__(self, metros_quadrados, n_camas, n_banheiros, n_historias, garagem, cerca):
+        super().__init__(metros_quadrados, n_camas, n_banheiros)
+        self.n_historias = n_historias
+        self.garagem = garagem
+        self.cerca = cerca 
 
 
-
-
-class Agent:
-    pass
